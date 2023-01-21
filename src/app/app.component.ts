e33781be0c762ce16a1eda0a467d483359266a17
Clark Mercer <clark.mercer@collins.com>
2023-01-21 16:25:14 -0500

    chess movement on two clicks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,87 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CheckersGame';
+  board = [
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [-1, 0, -1, 0, -1, 0, -1, 0],
+    [0, -1, 0, -1, 0, -1, 0, -1],
+    [-1, 0, -1, 0, -1, 0, -1, 0]
+  ];
+  currentPos: any;
+
+  onPieceClick(piece:number, i:number, j:number) {
+    if (this.currentPos) {
+      this.moveChecker(this.board, this.currentPos, { x: this.currentPos.x+1, y: this.currentPos.y+1 })
+      this.currentPos = null;
+    } 
+    else if (!this.currentPos && (piece === 1 || piece === -1)) {
+      this.currentPos = { x: i, y: j };
+    }
+  }
+
+  moveChecker(board:any, currentPos:any, newPos:any) {
+
+    // Here is an example of how the function could be used:
+    // const currentPos = { x: 3, y: 4 };
+    // const newPos = { x: 4, y: 5 };
+    // const updatedBoard = moveChecker(board, currentPos, newPos);
+    
+      // check if currentPos and newPos are valid positions on the board
+      if (currentPos.x < 0 || currentPos.x > 7 || currentPos.y < 0 || currentPos.y > 7 ||
+          newPos.x < 0 || newPos.x > 7 || newPos.y < 0 || newPos.y > 7) {
+          return "Invalid position";
+      }
+      // check if there is a checker at currentPos
+      if (board[currentPos.x][currentPos.y] === 0) {
+          return "No checker at current position";
+      }
+
+      let checker = board[currentPos.x][currentPos.y]
+
+      if(checker > 1 ){
+          // check if newPos is a valid move for the king
+          if(Math.abs(newPos.x - currentPos.x) !== Math.abs(newPos.y - currentPos.y) ) {
+              return "Invalid move for king";
+          }
+      }else{
+          // check if newPos is a valid move for the checker
+          if (Math.abs(newPos.x - currentPos.x) !== 1 || Math.abs(newPos.y - currentPos.y) !== 1) {
+              return "Invalid move";
+          }
+      }
+
+      // check if a capture is being made
+      const isCapture = Math.abs(newPos.x - currentPos.x) === 2 && Math.abs(newPos.y - currentPos.y) === 2;
+      if (isCapture) {
+          // calculate the position of the captured piece
+          const capturedPos = {
+              x: (currentPos.x + newPos.x) / 2,
+              y: (currentPos.y + newPos.y) / 2
+          };
+          // check if the captured piece is of the opposite color
+          if (board[capturedPos.x][capturedPos.y] === 0 || board[capturedPos.x][capturedPos.y] === checker) {
+              return "Invalid capture";
+          }
+          // remove the captured piece from the board
+          board[capturedPos.x][capturedPos.y] = 0;
+      }
+
+      // check if the checker reaches the opposite side of the board
+      if (checker > 0 && newPos.x === 0) {
+          checker = checker + 2;
+      } else if (checker < 0 && newPos.x === 7) {
+          checker = checker - 2;
+      }
+
+      // move the checker
+      board[newPos.x][newPos.y] = checker;
+      board[currentPos.x][currentPos.y] = 0;
+
+      return board;
+  }
+
 }
